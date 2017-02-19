ef901284253d219a1a46498271a2b228e5c5a67c
nancy-zhang <nzhang@wellesley.edu>
2017-02-19 17:31:04 -0500

    Update and rename hw1-3.pyx to hw13.pyx

--- hw13.pyx
+++ hw13.pyx
@@ -6,7 +6,7 @@ sizes = [2**6, 2**10, 2**20, 2**32]
 lists = [[1]*size for size in sizes] #each element is a list of specified size
 
 
-def summmation(a):
+def serial_summation(a):
     sums = a[0]
     i = 1
     N = len(a)
@@ -15,5 +15,8 @@ def summmation(a):
         i += 1
         
     return sums
+
+
+
 
 
