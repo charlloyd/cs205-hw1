0112c632ac0efb4b230b7226e27cd2d0f9dd2e38
eifer4 <edunipace@g.harvard.edu>
2017-02-19 18:47:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy
 import hw13
 
 # test cases
-# [2**6, 2**10, 2**20]#,
 sizes =  [2**6, 2**10, 2**20, 2**32]
 lists = [[1]*size for size in sizes] #each element is a list of specified size
 
