11ae4d37bfa5175de73d289c69ca0779b6fe6609
eifer4 <edunipace@g.harvard.edu>
2017-02-19 18:34:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import hw13
 
 # test cases
 # [2**6, 2**10, 2**20]#,
-sizes =  2**32
+sizes =  [2**6, 2**10, 2**20, 2**32]
 lists = [[1]*size for size in sizes] #each element is a list of specified size
 
 print(hw13.serial_summation(lists[0]))
