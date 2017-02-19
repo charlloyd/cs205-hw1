5687bc695a5d697446a58ba5376b90a476071d8b
eifer4 <edunipace@g.harvard.edu>
2017-02-19 17:52:05 -0500

    Update hw13.pyx

--- hw13.pyx
+++ hw13.pyx
@@ -1,11 +1,4 @@
-# Summation file to cythonize
-import numpy
-
-# test cases
-sizes = [2**6, 2**10, 2**20, 2**32]
-lists = [[1]*size for size in sizes] #each element is a list of specified size
-
-
+# Serial summation
 def serial_summation(a):
     sums = a[0]
     i = 1
