b52dd5da9c3460e0753f4be3a39c73671b6bc826
eifer4 <edunipace@g.harvard.edu>
2017-02-19 17:11:10 -0500

    Update hw1-3.pyx

--- hw1-3.pyx
+++ hw1-3.pyx
@@ -4,8 +4,6 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
-import cython
 import numpy
 
 
@@ -19,34 +17,4 @@ def summmation(a):
         
     return sums
 
-def countpennies(n,ct):
 
-    # first phase - everyone adds n times
-    individual_count = 256 // n
-    leftovers = 256 % n
-    elapsed = individual_count - 1
-    
-    # middle phase - some people add leftovers and some add theirs together
-    togethers = n - leftovers
-    if leftovers!=0:
-        togethers = (togethers // 2) + (togethers % 2) + leftovers
-        elapsed += 1
-        if ct==1:
-            elapsed += 1
-
-    # finish adding togethers
-    while togethers > 1:
-        togethers = (togethers // 2) + (togethers % 2)
-        elapsed += 1
-        if ct==1:
-            elapsed += 1
-
-    return elapsed
-
-print('done')
-
-n = list(range(1,150))
-t = []
-
-for i in n: 
-    t.append(countpennies(i,0))
