3d9996fff934927cdc27657164209c813ba67206
eifer4 <edunipace@g.harvard.edu>
2017-02-19 18:24:09 -0500

    Update modules.sh

--- modules.sh
+++ modules.sh
@@ -1,5 +1,5 @@
-module load python
+module load python/3.4.1-fasrc01
 module load legacy/0.0.1-fasrc01
-module load centos6/cython-0.21.1_python-2.7.8
+module load centos6/cython-0.20_python-3.3.2
 module load gcc/6.2.0-fasrc02
 
