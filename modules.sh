03768557d6ef260f1ed2663cd0d3a16a759415db
eifer4 <edunipace@g.harvard.edu>
2017-02-19 18:44:39 -0500

    Update modules.sh

--- modules.sh
+++ modules.sh
@@ -4,4 +4,5 @@ module load legacy/0.0.1-fasrc01
 module load centos6/cython-0.20_python-3.3.2
 module load gcc/6.2.0-fasrc02
 
-srun -p interact --pty --mem 20000 -t 0-06:00 /bin/bash
+srun -p interact --pty --mem 40000 -t 0-06:00 /bin/bash
+python setup.py build_ext --inplace
