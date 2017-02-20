2aa99ce3283e7fcf40235e208d806286e073a0e2
eifer4 <edunipace@g.harvard.edu>
2017-02-19 22:38:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 
+ext_modules = [
+    Extension(
+        "hw13",
+        ["hw13.pyx"],
+        extra_compile_args=['-fopenmp'],
+        extra_link_args=['-fopenmp'],
+    )
+]
+
 setup(
-    ext_modules=cythonize("hw13.pyx")
+    name='hw13',
+    ext_modules=cythonize(ext_modules),
 )
