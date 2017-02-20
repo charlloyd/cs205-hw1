49200d9a51e47bd5716f2d491f1534cdd1f7ff10
eifer4 <edunipace@g.harvard.edu>
2017-02-19 19:50:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import numpy
+import numpy as np
 import hw13
 
 # test cases
 sizes =  [2**6, 2**10, 2**20, 2**32]
 lists = [[1]*size for size in sizes] #each element is a list of specified size
+myarray = np.asarray(lists[0])
+
+for i in range(len(lists)):
+  print(hw13.parallel_sum(lists[i]))
 
 for i in range(len(lists)):
   print(hw13.serial_summation(lists[i]))
