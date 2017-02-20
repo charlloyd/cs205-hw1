3abc13a9b7f019b04da4b37b955a09f5567e74f2
eifer4 <edunipace@g.harvard.edu>
2017-02-19 22:41:00 -0500

    Update hw13.pyx

--- hw13.pyx
+++ hw13.pyx
@@ -29,14 +29,10 @@ def parallel_sum(long [:] a):
     N = a.shape[0]
     sums = a[N-1]
     
-#    for i in prange(N-1, nogil=True, schedule='dynamic'):
-#        sums += a[i];
-#    return sums;
+    for i in prange(N-1, nogil=True, num_threads=16, schedule='static'):
+        sums += a[i];
+    return sums;
     
-    with nogil, parallel(num_threads=16):
-        for i in prange(N-1, schedule='dynamic'):
-            sums += a[i]
-    return sums
 
 # Optimize this parallelization
 # adjust the number of threads to make the algorithm cost optimal
