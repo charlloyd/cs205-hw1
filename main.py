0dfd8c52c98058693890b3716f84b97189757c11
eifer4 <edunipace@g.harvard.edu>
2017-02-19 20:14:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import numpy as np
 import hw13
+import time
 
 # test cases
 sizes =  [2**6, 2**10, 2**20, 2**32]
-lists = [np.asarray([1]*size) for size in sizes] #each element is a list of specified size
+#lists = [np.asarray([1]*size) for size in sizes] #each element is a list of specified size
 
-for i in range(len(lists)):
-  print(hw13.parallel_sum(lists[i]))
+parallel_timings = []
+serial_timings = []
+start = 0
 
-for i in range(len(lists)):
-  print(hw13.serial_summation(lists[i]))
+for i in range(len(sizes)):
+  myarray = np.asarray([1]*sizes[i])
+  start = time.time()
+  print(hw13.parallel_sum(myarray))
+  parallel_timings.append(time.time()-start)
+  start = time.time()
+  print(hw13.serial_summation(myarray))
+  serial_timings.append(time.time()-start)
+  
+with open("parallel.csv", 'w') as f:
+    for s in parallel_timings:
+        f.write(s + '\n')
+
+with open("parallel.csv", 'r') as f:
+    parallel_timings = [line.rstrip('\n') for line in f]
