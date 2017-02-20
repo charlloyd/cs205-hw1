1d6e48b78f800c6ade93fff15b7e0ec01ed019fd
eifer4 <edunipace@g.harvard.edu>
2017-02-19 23:33:10 -0500

    Update hw13.pyx

--- hw13.pyx
+++ hw13.pyx
@@ -1,15 +1,13 @@
 #!python
 #cython: boundscheck=False, wraparound=False, nonecheck=False
-#distutils: language = c++
 
 from cython.parallel import parallel, prange
-from libcpp.vector cimport vector
 
 # DON'T USE NEGATIVE INDEXING!!! Turning this option off makes code faster, 
 # but means python style negative indexing will cause segfaults
 
 # Serial summation
-cpdef long serial_summation(vector[long]& a):
+cpdef long serial_summation(long[:] a):
     cdef long sums = a[0]
     cdef size_t i
     
@@ -19,7 +17,7 @@ cpdef long serial_summation(vector[long]& a):
     return sums
 
 # Parallelize summation using Cython
-cpdef long parallel_sum(vector[long]& a) nogil:
+cpdef long parallel_sum(long[:] a) nogil:
     cdef long sums = a[0]
     cdef size_t i
     
