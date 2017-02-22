e58d4a3efbb799d8921cbc361cbffe92f242eafc
Eric Dunipace <eifer@Cid.local>
2017-02-22 16:16:53 -0500

    First commit

--- hw13.pyx
+++ hw13.pyx
@@ -1,7 +1,9 @@
 #!python
 #cython: boundscheck=False, wraparound=False, nonecheck=False
+#cython: --compile-args=-fopenmp --link-args=-fopenmp --force -a
 
-from cython.parallel import parallel, prange
+from cython.parallel import parallel, prange, threadsavailable, threadid
+from
 
 # DON'T USE NEGATIVE INDEXING!!! Turning this option off makes code faster, 
 # but means python style negative indexing will cause segfaults
@@ -17,11 +19,11 @@ cpdef long serial_summation(long[:] a):
     return sums
 
 # Parallelize summation using Cython
-cpdef long parallel_sum(long[:] a) nogil:
+cpdef long parallel_sum(long[:] a):
     cdef long sums = a[0]
     cdef size_t i
     
-    for i in prange(1, a.shape[0], nogil=True, num_threads=8, schedule='dynamic'):
+    for i in prange(1, a.shape[0], nogil=True, schedule='dynamic'):
         sums += a[i];
     return sums;
     
@@ -29,7 +31,35 @@ cpdef long parallel_sum(long[:] a) nogil:
 # Optimize this parallelization
 # adjust the number of threads to make the algorithm cost optimal
 
-        
-        
+# Attempt at more cost effective Sum
+cpdef long parallel_sum_thread(long[:] data):
+    cdef double* buf = <double*>malloc(threadsavailable(schedule='dynamic') * sizeof(double))
+    cdef double* threadbuf
+    cdef long sums = 0
+    cdef int N = data.shape[0]
+    cdef size_t i
+    cdef threadlocal(double) temp_sum = 0
+
+    with nogil, parallel:
+        tid = threadid()
+#       threadbuf = buf + threadid() # thread setup
+
+        for s in prange(N/2, schedule='dynamic'):
+            if (tid < s) {
+                data[tid] += data[tid + s];
+            }
+            _syncthreads();
+
+        if (tid < 32)   {
+            data[tid] += data[tid + 32];
+            data[tid] += data[tid + 16];
+            data[tid] += data[tid + 8];
+            data[tid] += data[tid + 4];
+            data[tid] += data[tid + 2];
+            data[tid] += data[tid + 1];
+        }
+
+    return sums;
+
 
 
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import csv
 sizes =  [2**6, 2**10, 2**20, 2**32]
 #lists = [np.asarray([1]*size) for size in sizes] #each element is a list of specified size
 
-parallel_timings = []
+parallel_timings_naive = []
+parallel_timings_thread = []
 serial_timings = []
 start = 0
 
@@ -15,7 +16,10 @@ for i in range(len(sizes)):
   myarray = np.ones((sizes[i],), dtype=np.int_)
   start = time.time()
   print(hw13.parallel_sum(myarray))
-  parallel_timings.append(time.time()-start)
+  parallel_timings_naive.append(time.time()-start)
+  start = time.time()
+  print(hw13.parallel_sum_thread(myarray))
+  parallel_timings_thread.append(time.time()-start)
   start = time.time()
   print(hw13.serial_summation(myarray))
   serial_timings.append(time.time()-start)
@@ -25,6 +29,7 @@ print(["Serial: ",serial_timings])
   
 with open("timings.csv", 'w', newline='') as f:
     writer = csv.writer(f, delimiter = ',')
-    writer.writerow([str(i) for i in parallel_timings])
+    writer.writerow([str(i) for i in parallel_timings_naive])
+    writer.writerow([str(i) for i in parallel_timings_thread])
     writer.writerow([str(i) for i in serial_timings])
     f.close()
