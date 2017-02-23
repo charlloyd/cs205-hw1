e828ea70ba361686e45e76a122331a1dcd5572de
Eric Dunipace <eifer@Cid.local>
2017-02-23 16:49:19 -0500

    "2/23/2017"

--- modules.sh
+++ modules.sh
@@ -4,5 +4,5 @@ module load legacy/0.0.1-fasrc01
 module load centos6/cython-0.20_python-3.3.2
 module load gcc/6.2.0-fasrc02
 
-srun -p general -N 1 -c 4 --pty --mem 5000 -t 0-06:00 /bin/bash
+srun --mem-per-cpu=4000 -p general -n 64 --pty -t 0-06:00 /bin/bash
 python setup.py build_ext --inplace
