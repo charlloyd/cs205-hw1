b5b052661f155d2564e2db375f9436d7716eb0ab
charlloyd <char.lloyd@gmail.com>
2017-02-26 15:25:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ parallel_timings_naive = []
 parallel_timings_thread = []
 serial_timings = []
 
-
-
 parallel_result_naive =[]
 parallel_result_thread=[]
 serial_result=[]
@@ -25,7 +23,6 @@ nthreads = int(os.environ.get('SLURM_ARRAY_TASK_ID'))
 nthreads = int('64')
 
 iter = range(len(sizes))
-
 
 for i in iter:
     myarray = np.ones((sizes[i],), dtype=np.int_)
@@ -44,8 +41,6 @@ parallel_spd_thread = [serial_timings[i]/parallel_timings_thread[i] for i in ite
 parallel_eff_naive = [parallel_spd_naive[i]/nthreads for i in iter ]
 parallel_eff_thread = [parallel_spd_thread[i]/nthreads for i in iter ]
 
-
-
 parallel_timings_naive.insert(0,"Parallel Naive Times")
 parallel_timings_thread.insert(0,"Parallel Chunked Times")
 serial_timings.insert(0,"Serial Times")
@@ -53,8 +48,6 @@ parallel_spd_naive.insert(0,"Parallel Naive Speed-up")
 parallel_spd_thread.insert(0,"Parallel Chunked Speed-up")
 parallel_eff_naive.insert(0,"Parallel Naive Efficiency")
 parallel_eff_thread.insert(0,"Parallel Chunked Efficiency")
-
-
 
 colnames = ["Algorithm"]
 colnames.append(sizes)
@@ -71,7 +64,6 @@ parallel_eff_thread.append(parallel_timings_thread[-1])
 filename_time = "sum_timings_nthread_" + str(nthreads) + ".csv"
 filename_spd = "sum_spd_nthread_" + str(nthreads) + ".csv"
 filename_eff = "sum_eff_nthread_" + str(nthreads) + ".csv"
-
 
 with open(filename_time, 'w', newline='') as f:
     writer = csv.writer(f, delimiter = ',')
@@ -98,7 +90,5 @@ with open(filename_eff, 'w', newline='') as f:
     writer.writerow([str(i) for i in parallel_eff_naive])
     writer.writerow([str(i) for i in parallel_eff_thread])
     f.close()
-
-
-
+    
 exit()
