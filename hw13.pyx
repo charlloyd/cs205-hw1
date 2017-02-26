551f1ddb9e39a88b57e19ba8f3a435f72e089b97
charlloyd <char.lloyd@gmail.com>
2017-02-26 16:30:37 -0500

    Update hw13.pyx

--- hw13.pyx
+++ hw13.pyx
@@ -47,39 +47,3 @@ cpdef long parallel_sum_thread(long[::] data, int nthreads):
         sums += data[s]
         
     return sums
-
-###########################
-# 4. matrix multiplication
-###########################
-
-cpdef double matMult_naive(double[::,::] mat, double[:] vec, int nthreads):
-    cdef unsigned int N = vec.shape[0]
-    cdef unsigned int J = mat.shape[1]
-    cdef unsigned int s,j, n
-    cdef unsigned int chunk = N/nthreads
-    cdef double out[N]
-
-    for n in range(N):
-        out[n] = 0
-
-    for s in prange(N, nogil=True, num_threads=nthreads, schedule='dynamic'):
-        for j in prange(J, num_threads=nthreads, schedule='dynamic'):
-            out[s] += mat[s,j] * vec[j]
-
-    return out
-
-cpdef double matMult_thread(double[::,::] mat, double[:] vec, int nthreads):
-    cdef unsigned int N = vec.shape[0]
-    cdef unsigned int J = mat.shape[1]
-    cdef unsigned int s, j, n
-    cdef unsigned int chunk = N/nthreads
-    cdef double out[N]
-
-    for n in range(N):
-        out[n] = 0
-
-    for s in prange(0, N, nogil=True, num_threads=nthreads, chunksize=chunk, schedule='guided'):
-        for j in range(J):
-            out[s] += mat[s,j] * vec[j]
-
-    return out
