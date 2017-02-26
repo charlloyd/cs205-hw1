bab66662b00696bc652f9dfe2752c8e40d843f49
charlloyd <char.lloyd@gmail.com>
2017-02-26 17:27:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 
+our_modules = [
+    Extension("hw13", ["hw13.pyx"], extra_compile_args=['-fopenmp'], extra_link_args=['-fopenmp']),
+    #Extension("hw14", ["hw14.pyx"], extra_compile_args=['-fopenmp'], extra_link_args=['-fopenmp']),
+]
+
 setup(
   name = 'hw1',
-  ext_modules=[
-    Extension("hw13", ["hw13.pyx"], extra_compile_args=['-fopenmp'], extra_link_args=['-fopenmp']),
-    Extension("hw14", ["hw14.pyx"], extra_compile_args=['-fopenmp'], extra_link_args=['-fopenmp']),
-    ],
-  cmdclass = {'build_ext': build_ext}
+  ext_modules=cythonize(our_modules),
+  #cmdclass = {'build_ext': build_ext}
 )
