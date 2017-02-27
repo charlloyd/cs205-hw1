4bfb950b96fde370fb609ab10dcf9623ab9e2210
charlloyd <char.lloyd@gmail.com>
2017-02-27 17:25:19 -0500

    Update hw14.pyx

--- hw14.pyx
+++ hw14.pyx
@@ -1,4 +1,3 @@
-
 #!python
 #cython: boundscheck=False, wraparound=False, nonecheck=False
 #cython: --compile-args=-fopenmp --link-args=-fopenmp --force -a
@@ -8,14 +7,8 @@ from cython.operator cimport dereference as deref
 from libc.stdlib cimport malloc, free, rand
 
 
-# DON'T USE NEGATIVE INDEXING!!! Turning this option off makes code faster, 
-# but means python style negative indexing will cause segfaults
-
-
-
-
 ###########################
-# 4. matrix multiplication
+# 3. matrix-vector multiplication
 ###########################
 
 cpdef int matMult_serial(double[::,::] X, double[::,::] Y, double[::,::] out, int nthreads):
@@ -41,7 +34,6 @@ cpdef int matMult_naive(double[::,::] X, double[::,::] Y, double[::,::] out, int
             for j in range(J):
                 out[n,j] += X[n,j] * Y[j,k]
     return 0
-
 
 cpdef int matMult_thread(double[::,::] X, double[::,::] Y, double[::,::] out, int nthreads):
     cdef unsigned int N = X.shape[0]
