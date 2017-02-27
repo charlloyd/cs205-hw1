864dce021035c2192c59aeaf2263b291507e46e0
charlloyd <char.lloyd@gmail.com>
2017-02-27 17:24:46 -0500

    Update hw14.pyx

--- hw14.pyx
+++ hw14.pyx
@@ -1,50 +1,57 @@
+
 #!python
 #cython: boundscheck=False, wraparound=False, nonecheck=False
 #cython: --compile-args=-fopenmp --link-args=-fopenmp --force -a
 
 from cython.parallel cimport parallel, prange, threadid
 from cython.operator cimport dereference as deref
-from libc.stdlib cimport malloc, free
+from libc.stdlib cimport malloc, free, rand
+
 
 # DON'T USE NEGATIVE INDEXING!!! Turning this option off makes code faster, 
 # but means python style negative indexing will cause segfaults
 
-###########################
-# 3. matrix vector multiplication
-###########################
 
-cpdef double matMult_naive(double[::,::] mat, double[:] vec, int nthreads):
-    cdef unsigned int N = vec.shape[0]
-    cdef unsigned int J = mat.shape[1]
-    cdef unsigned int s,j, n
-    cdef unsigned int chunk = N/nthreads
-    cdef double out[N]
 
-    for n in range(N):
-        out[n] = 0
-
-    for s in prange(N, nogil=True, num_threads=nthreads, schedule='dynamic'):
-        for j in prange(J, num_threads=nthreads, schedule='dynamic'):
-            out[s] += mat[s,j] * vec[j]
-
-    return out
-
-cpdef double matMult_thread(double[::,::] mat, double[:] vec, int nthreads):
-    cdef unsigned int N = vec.shape[0]
-    cdef unsigned int J = mat.shape[1]
-    cdef unsigned int s, j, n
-    cdef unsigned int chunk = N/nthreads
-    cdef double out[N]
-
-    for n in range(N):
-        out[n] = 0
-
-    for s in prange(0, N, nogil=True, num_threads=nthreads, chunksize=chunk, schedule='guided'):
-        for j in range(J):
-            out[s] += mat[s,j] * vec[j]
-
-    return out
 
 ###########################
 # 4. matrix multiplication
 ###########################
+
+cpdef int matMult_serial(double[::,::] X, double[::,::] Y, double[::,::] out, int nthreads):
+    cdef unsigned int N = X.shape[0]
+    cdef unsigned int J = Y.shape[0]
+    cdef unsigned int K = Y.shape[1]
+    cdef unsigned int k, j, n
+
+    for n in range(N):
+        for k in range(K):
+            for j in range(J):
+                out[n,j] += X[n,j] * Y[j,k]
+    return 0
+
+cpdef int matMult_naive(double[::,::] X, double[::,::] Y, double[::,::] out, int nthreads):
+    cdef unsigned int N = X.shape[0]
+    cdef unsigned int J = Y.shape[0]
+    cdef unsigned int K = Y.shape[1]
+    cdef unsigned int k, j, n
+
+    for n in prange(N, nogil=True, num_threads=nthreads, schedule='dynamic'):
+        for k in range(K):
+            for j in range(J):
+                out[n,j] += X[n,j] * Y[j,k]
+    return 0
+
+
+cpdef int matMult_thread(double[::,::] X, double[::,::] Y, double[::,::] out, int nthreads):
+    cdef unsigned int N = X.shape[0]
+    cdef unsigned int J = Y.shape[0]
+    cdef unsigned int K = Y.shape[1]
+    cdef unsigned int k, j, n
+    cdef unsigned int chunk = N/nthreads
+
+    for n in prange(N, nogil=True, num_threads=nthreads, chunksize=chunk, schedule='guided'):
+        for k in range(K):
+            for j in range(J):
+                out[n,k] += X[n,j] * Y[j,k]
+    return 0
