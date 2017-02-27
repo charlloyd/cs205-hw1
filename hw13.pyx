bb76cb9c25f09e0b0ef5a90c157562027b3ee98b
Eric Dunipace <eifer@Cid.local>
2017-02-27 17:07:24 -0500

    who cares

--- hw13.pyx
+++ hw13.pyx
@@ -98,10 +98,11 @@ cpdef int vecmatMult_explicit(double[::,::] mat, double[::] vec, double[::] out,
     cdef double vecChunk = <double >(malloc (N * sizeof(double)))
     cdef double matChunk = <double >(malloc (N * chunk * sizeof(double)))
     cdef double temp = <double>(malloc (chunk * sizeof(double)))
-    cdef int *step[len(range(0,N, chunk))]
+    cdef int chunk_iter = range(0, N, chunk)
+    cdef int step[len(chunk_iter)]
 
-    for i in len(range(0,N,chunk)):
-        step[i] = range(0,N,chunk)[i]
+    for i in len(chunk_iter):
+        step[i] = chunk_iter[i]
     with nogil, parallel(num_threads=nthreads):
         tid = threadid()
         for f in range(chunk):
