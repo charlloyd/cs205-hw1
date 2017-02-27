7f8df68a8270cf6fca8e75b5bc8ddddd07bb501a
Eric Dunipace <eifer@Cid.local>
2017-02-27 17:05:49 -0500

    who cares

--- hw13.pyx
+++ hw13.pyx
@@ -98,7 +98,7 @@ cpdef int vecmatMult_explicit(double[::,::] mat, double[::] vec, double[::] out,
     cdef double vecChunk = <double >(malloc (N * sizeof(double)))
     cdef double matChunk = <double >(malloc (N * chunk * sizeof(double)))
     cdef double temp = <double>(malloc (chunk * sizeof(double)))
-    cdef int step[len(range(0,N, chunk))]
+    cdef int *step[len(range(0,N, chunk))]
 
     for i in len(range(0,N,chunk)):
         step[i] = range(0,N,chunk)[i]
