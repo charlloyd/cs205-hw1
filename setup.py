0786b8df782f0ffc0bd598e5d3c64ba6ba8b33fd
Eric Dunipace <eifer@Cid.local>
2017-02-28 16:36:35 -0500

    update

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from Cython.Distutils import build_ext
 ### list all .pyx files
 
 our_modules = [
-    Extension("hw13", ["hw13.pyx"], extra_compile_args=['-fopenmp'], extra_link_args=['-fopenmp']),
-    Extension("hw14", ["hw14.pyx"], extra_compile_args=['-fopenmp',], extra_link_args=['-fopenmp']),
-    Extension("hw14opt",["hw14.pyx"], extra_compile_args=['-fopenmp'], extra_link_args=['-fopenmp'])
+    Extension("hw13", ["hw13.pyx"], language="c++", extra_compile_args=['-fopenmp','-O3'], extra_link_args=['-fopenmp']),
+    Extension("hw14", ["hw14.pyx"], language="c++", extra_compile_args=['-fopenmp',], extra_link_args=['-fopenmp']),
+    Extension("hw14opt",["hw14.pyx"], language="c++", extra_compile_args=['-fopenmp','-O3'], extra_link_args=['-fopenmp'])
 ]
 
 ### apparently equivalent ways to do setup
 
-setup(name = 'hw1', ext_modules=cythonize(our_modules))
-#setup(name = 'hw1', cmdclass = {'build_ext': build_ext}, ext_modules = our_modules)
+#setup(name = 'hw1', ext_modules=cythonize(our_modules))
+setup(name = 'hw1', cmdclass = {'build_ext': build_ext}, ext_modules = our_modules)
