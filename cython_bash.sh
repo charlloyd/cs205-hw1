1e80640624b4cc51f889d0bd56740a02c582b670
charlloyd <char.lloyd@gmail.com>
2017-02-27 21:51:53 -0500

    Update cython_bash.sh

--- cython_bash.sh
+++ cython_bash.sh
@@ -13,6 +13,7 @@ module load python/3.4.1-fasrc01
 module load legacy/0.0.1-fasrc01
 module load centos6/cython-0.20_python-3.3.2
 module load gcc/6.2.0-fasrc02
+module load centos6/pandas-0.11.0_python-2.7.3
 
 ## EXECUTE CODE ##
 python setup.py build_ext --inplace
