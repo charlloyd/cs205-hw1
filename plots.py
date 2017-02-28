3c3181ae2ce81489e6a094dd05aecc05a62eaaf8
charlloyd <char.lloyd@gmail.com>
2017-02-27 21:52:19 -0500

    Update plots.py

--- plots.py
+++ plots.py
@@ -1,9 +1,7 @@
-# don't forget to "module load legacy/0.0.1-fasrc01; module load centos6/pandas-0.11.0_python-2.7.3" in shell
-
 import pandas as pd
-import matplotlib.pyplot as plt
-import math
-import matplotlib.patches as mpatches
+#import matplotlib.pyplot as plt
+#import math
+#import matplotlib.patches as mpatches
 
 sums = pd.read_csv('summation.csv')
 matvec = pd.read_csv('matvec.csv')
