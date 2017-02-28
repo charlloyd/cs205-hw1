a4965b0c79eaf2a87081f2e35fc4b0bb31d88c03
charlloyd <char.lloyd@gmail.com>
2017-02-27 22:20:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ for n in nthreads:
         step = [idx for idx in range(0,sizes[i],chunk)]
         step = np.array(step, dtype=np.intc)
         start = time.time()
-        hw13.vecmatMult_thread(mymat, myvec, outvec, n)
+        #hw13.vecmatMult_thread(mymat, myvec, outvec, n)
         parallel_timings_thread.append(time.time()-start)
         parallel_result_thread.append(outvec)
         
