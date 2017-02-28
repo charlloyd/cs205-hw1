950db5daced1ae5b91b92da7445f03122750a120
Eric Dunipace <eifer@Cid.local>
2017-02-28 13:04:22 -0500

    update

--- main.py
+++ main.py
@@ -212,56 +212,6 @@ for n in nthreads:
         writer.writerow([str(i) for i in parallel_eff_thread])
         f.close()
 
-    ###########################
-    # HW 1 QUESTION 4
-    ###########################
-
-    start = []
-    dgemm_time = []
-    serial_time = []
-    gflopsPerSec = []
-    operations = []
-    parallel_time_naive = []
-    parallel_time_block = []
-
-    for i in iter:
-        random.seed(5555)
-        X = Y = outmat = np.zeros((sizes[i],sizes[i]))
-        
-        operations.append(2 * (i**3))
-        
-        for j in range(X.shape[0]):
-            for k in range(X.shape[1]):
-                X[j,k] = random.gauss(0,1)
-                Y[j,k] = random.gauss(0,1)
-
-        # Linear comparison between dgemm and cython function
-        start = time.time()
-        dgemm(alpha=1.,a=X,b=Y)
-        dgemm_time.append(time.time()-start)
-        
-        start = time.time()
-        hw14.matMult_serial(X, Y, outmat, n)
-        serial_time.append(time.time()-start)
-        
-        #Naive parallel algorithm without blocking
-        outmat = np.zeros((sizes[i],sizes[i]))
-        row = round(23*100*1000 / 8/(sizes[i]/2))
-        chunk = (2*(sizes[i]**2))/(row**2)
-        if chunk < n:
-            chunk = n
-        start = time.time()
-        hw14.matMult_thread(X, Y, outmat, n, chunk)
-        parallel_time_naive.append(time.time() - start)
-        
-        #Naive parallel algorithm with blocking
-        outmat = np.zeros((sizes[i],sizes[i]))
-        row = round(23*100*1000 / 8/(sizes[i]/2))
-        chunk = (2*(sizes[i]**2))/(row**2)
-        if chunk < n:
-            chunk = n
-        start = time.time()
-        hw14.matMult_block(X, Y, outmat, n, chunk)
-        parallel_time_naive.append(time.time() - start)
+    
 
 exit()
