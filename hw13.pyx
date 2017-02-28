5063eb7cab8c6003f0648448b0769915a78d7678
Eric Dunipace <eifer@Cid.local>
2017-02-27 19:05:25 -0500

    update

--- hw13.pyx
+++ hw13.pyx
@@ -92,7 +92,7 @@ cpdef int vecmatMult_thread(double[::,::] mat, double[::] vec, double[::] out, i
 cpdef int vecmatMult_explicit(double[::,::] mat, double[::] vec, double[::] out, int nthreads, int[:] step, int chunk):
     cdef unsigned int N = vec.shape[0]
     cdef unsigned int J = mat.shape[1]
-    cdef size_t j, k, f, g, s, t, v
+    cdef size_t j, k, f, g, t, v
     cdef int tid;
     cdef double *vecChunk
     cdef double *matChunk
