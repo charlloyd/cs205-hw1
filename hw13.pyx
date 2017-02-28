9a67b20f232ad0474b235f4fd3625378bbab8503
Eric Dunipace <eifer@Cid.local>
2017-02-28 16:33:31 -0500

    update

--- hw13.pyx
+++ hw13.pyx
@@ -1,4 +1,5 @@
 #!python
+#cython: boundscheck=False, wraparound=False, nonecheck=False
 #cython: --compile-args=-fopenmp --link-args=-fopenmp --force -a
 # distutils: extra_compile_args = -fopenmp
 # distutils: extra_link_args = -fopenmp
@@ -14,9 +15,6 @@ from cython.parallel cimport parallel, prange, threadid
 from cython.operator cimport dereference as deref
 cimport numpy as np
 
-
-# DON'T USE NEGATIVE INDEXING!!! Turning this option off makes code faster, 
-# but means python style negative indexing will cause segfaults
 
 ###########################
 # SUMMATION
