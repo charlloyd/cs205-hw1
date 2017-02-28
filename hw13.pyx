c3cae16d4d7a0dae4e094ff0d9489db55046c154
Eric Dunipace <eifer@Cid.local>
2017-02-28 16:19:39 -0500

    update

--- hw13.pyx
+++ hw13.pyx
@@ -1,5 +1,8 @@
 #!python
 #cython: boundscheck=False, wraparound=False, nonecheck=False
+#cython: --compile-args=-fopenmp --link-args=-fopenmp --force -a
+# distutils: extra_compile_args = -fopenmp
+# distutils: extra_link_args = -fopenmp
 # distutils: language = c++
 
 
