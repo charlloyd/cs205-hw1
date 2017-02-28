bbee6ca782296980ba860262664d3b26607ca614
charlloyd <char.lloyd@gmail.com>
2017-02-27 21:40:37 -0500

    Update plots.py

--- plots.py
+++ plots.py
@@ -1,1 +1,12 @@
 #stuff
+import pandas
+import matplotlib.pyplot as plt
+
+sums = pd.read_csv('summation.csv')
+matvec = pd.read_csv('matvec.csv')
+
+y_sum = ['2^6','2^10','2^20']
+y_matvec = ['2^6','2^10']
+
+sums
+matvec
