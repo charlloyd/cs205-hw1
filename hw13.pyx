5234f569276fbfb4564de3f93be57574cc4c863c
Eric Dunipace <eifer@Cid.local>
2017-02-28 00:02:44 -0500

    update

--- hw13.pyx
+++ hw13.pyx
@@ -63,7 +63,7 @@ cpdef int parallel_sum_block(long[::] data, int nthreads, int[:] step, int chunk
 
     with nogil, parallel(num_threads=nthreads):
         tid = threadid()
-        sdata = <long*>(malloc(2*chunk * sizeof(long)))
+        sdata = <long*>(malloc(32*chunk * sizeof(long)))
         temp_sum = <long*>(malloc(sizeof(long)))
 
         for j in range(chunk):
--- hw14.pyx
+++ hw14.pyx
@@ -5,6 +5,7 @@
 from cython.parallel cimport parallel, prange, threadid
 from cython.operator cimport dereference as deref
 from libc.stdlib cimport malloc, free, rand
+cimport numpy as np
 
 
 ###########################
@@ -48,7 +49,7 @@ cpdef int matMult_thread(double[::,::] X, double[::,::] Y, double[::,::] out, in
                 out[n,k] += X[n,j] * Y[j,k]
     return 0
 
-cpdef int matMult_block(double[::,::] X, double[::,::] Y, double[::,::] out, int nthreads, int[:,:] step1, int S, int[:,:] step2, int T, int chunk):
+cdef int mmb(double[::,::] X, double[::,::] Y, double[::,::] out, int nthreads, int[:,:] step, int S, int T, int chunk):
     cdef unsigned int N = X.shape[0]
     cdef unsigned int J = Y.shape[0]
     cdef unsigned int K = Y.shape[1]
@@ -67,15 +68,24 @@ cpdef int matMult_block(double[::,::] X, double[::,::] Y, double[::,::] out, int
         for t in range(T):
             for s in range(S):
                 for a in range(chunk):
-                    C[a] = 0
                     for b in range(J):
-                        A[a*J + b] = X[a + step1[tid,s],b]
+                        A[a*J + b] = X[a + step[tid,s],b]
                         B[a*J + b] = Y[b,t]
                 for k in range(chunk):
                     for j in range(J):
                         C[k] = C[k] + A[k*J + j] * B[k*J + j]
                 for n in prange(nthreads):
                     for k in range(chunk):
-                        out[k + step1[tid,s],t] +=  C[k]
+                        out[k + step[tid,s],t] +=  C[k]
+        free(A)
+        free(B)
+        free(C)
     return 0
 
+def matMult_block(double[::,::] X, double[::,::] Y, double[::,::] out, int nthreads, int[:,:] step, int S, int T, int chunk)
+    cdef int chunk = round(23*100*1000 / 8/(sizes[i]*2))
+    step = [idx for idx in range(0,sizes[i],chunk)]
+    step = np.array(step, dtype=np.intc)
+    step
+
+    mmb(
