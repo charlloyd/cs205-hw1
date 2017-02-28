7f6fedf326273cbbfdcac2e02bc3be553c057883
Eric Dunipace <eifer@Cid.local>
2017-02-27 21:55:10 -0500

    update

--- hw14.pyx
+++ hw14.pyx
@@ -48,16 +48,34 @@ cpdef int matMult_thread(double[::,::] X, double[::,::] Y, double[::,::] out, in
                 out[n,k] += X[n,j] * Y[j,k]
     return 0
 
-cpdef int matMult_block(double[::,::] X, double[::,::] Y, double[::,::] out, int nthreads, int[:] step, int chunk):
+cpdef int matMult_block(double[::,::] X, double[::,::] Y, double[::,::] out, int nthreads, int[:,:] step1, int S, int[:,:] step2, int T, int chunk):
     cdef unsigned int N = X.shape[0]
     cdef unsigned int J = Y.shape[0]
     cdef unsigned int K = Y.shape[1]
-    cdef unsigned int k, j, n
+    cdef unsigned int a, b, k, j, n, s,t
     cdef unsigned int chunk = N/nthreads
+    cdef int *tid
+    cdef double *A
+    cdef double *B
+    cdef double *C
 
-    for n in prange(N, nogil=True, num_threads=nthreads, chunksize=chunk, schedule='guided'):
-        for k in range(K):
-            for j in range(J):
-                out[n,k] += X[n,j] * Y[j,k]
+    with nogil, parallel(num_threads = nthreads):
+        tid = threadid()
+        A = <double *>(malloc (J * chunk * sizeof(double)))
+        B = <double *>(malloc (J * chunk * sizeof(double)))
+        C = <double *>(malloc (chunk * sizeof(double)))
+        for t in T:
+            for s in S:
+                for a in range(chunk):
+                    C[a] = 0
+                    for b in range(J):
+                        A[a*J + b] = X[a + step1[tid,s],b]
+                        B[a*J + b] = Y[b,t]
+                for k in range(chunk):
+                    for j in range(J):
+                        C[k] = C[k] + A[k*J + j] * B[k*J + j]
+                for n in prange(nthreads):
+                    for k in range(chunk):
+                        out[k + step1[tid,s],t] += out[k + step1[tid,s],t] + C[k]
     return 0
 
--- main.py
+++ main.py
@@ -206,7 +206,7 @@ for n in nthreads:
     
     # write results to csv
     fn_mat = "mat_timings_nthread_" + str(n) + ".csv"
-    with open(fn_sum, 'a', newline='') as f:
+    with open(fn_mat, 'a', newline='') as f:
         writer = csv.writer(f, delimiter = ',')
         writer.writerow([str(i) for i in colnames])
         writer.writerow([str(i) for i in serial_timings])
@@ -218,101 +218,6 @@ for n in nthreads:
         writer.writerow([str(i) for i in parallel_eff_thread])
         f.close()
 
-    ### MATRIX VECTOR MULTIPLICATION ###
-    
-    # re-initialize arrays
-    serial_timings = []
-    parallel_timings_naive = []
-    parallel_timings_thread = []
-    serial_result=[]
-    parallel_result_naive =[]
-    parallel_result_thread=[]
-    compare = []
-    
-    # re-define sizes
-    sizes = [2**6, 2**10]#, 2**16]
-    iter = range(len(sizes))
-
-    # main matrix-vector multiplication loop
-    for i in iter:
-        random.seed(5555)
-        myvec = np.zeros((sizes[i],))
-        outvec = np.zeros_like(myvec)
-        mymat = np.zeros((sizes[i], sizes[i]))
-
-        # create a matrix and vector
-        for j in range(sizes[i]):
-            myvec[j] = random.gauss(0,1)
-            for k in range(sizes[i]):
-                mymat[j,k] = random.gauss(0,1)
-
-        # NP test
-        compare.append(np.dot(mymat, myvec))
-
-        # serial matrix-vector multiplication algorithm
-        start = time.time()
-        hw13.vecmatMult_serial(mymat, myvec, outvec)
-        serial_timings.append(time.time()-start)
-        serial_result.append(outvec)
-
-        # parallel naive matrix-vector multiplication algorithm
-        outvec = np.zeros_like(myvec)
-        start = time.time()
-        hw13.vecmatMult_naive(mymat, myvec, outvec, n)
-        parallel_timings_naive.append(time.time()-start)
-        parallel_result_naive.append(outvec)
-
-        # parallel thread ("guided") matrix-vector multiplication algorithm
-        outvec = np.zeros_like(myvec)
-        chunk = round(23*100*1000 / 8/(sizes[i]*2))
-        step = [idx for idx in range(0,sizes[i],chunk)]
-        step = np.array(step, dtype=np.intc)
-        start = time.time()
-        hw13.vecmatMult_thread(mymat, myvec, outvec, n)
-        parallel_timings_thread.append(time.time()-start)
-        parallel_result_thread.append(outvec)
-        
-    # timings
-    serial_timings.append(np.array_equal(sizes, serial_result))
-    parallel_timings_naive.append(np.array_equal(sizes, parallel_result_naive))
-    parallel_timings_thread.append(np.array_equal(sizes, parallel_result_thread))
-    
-    # speedup
-    parallel_spd_naive = [serial_timings[i]/parallel_timings_naive[i] for i in iter]
-    parallel_spd_naive.append(parallel_timings_naive[-1])
-    parallel_spd_thread = [serial_timings[i]/parallel_timings_thread[i] for i in iter]
-    parallel_spd_thread.append(parallel_timings_thread[-1])
-    
-    # efficiency
-    parallel_eff_naive = [parallel_spd_naive[i]/n for i in iter]
-    parallel_eff_naive.append(parallel_timings_naive[-1])
-    parallel_eff_thread = [parallel_spd_thread[i]/n for i in iter]
-    parallel_eff_thread.append(parallel_timings_thread[-1])
-
-    # prep before writing
-    colnames = ["Algorithm"]
-    colnames.append(sizes)
-    colnames.append("Pass")
-    serial_timings.insert(0,"Serial Times")
-    parallel_timings_naive.insert(0,"Parallel Naive Times")
-    parallel_timings_thread.insert(0,"Parallel Guided Times")
-    parallel_spd_naive.insert(0,"Parallel Naive Speed-up")
-    parallel_spd_thread.insert(0,"Parallel Guided Speed-up")
-    parallel_eff_naive.insert(0,"Parallel Naive Efficiency")
-    parallel_eff_thread.insert(0,"Parallel Guided Efficiency")
-    
-    # write results to csv
-    with open(fn_matvec, 'a', newline='') as f:
-        writer = csv.writer(f, delimiter = ',')
-        writer.writerow([str(i) for i in colnames])
-        writer.writerow([str(i) for i in serial_timings])
-        writer.writerow([str(i) for i in parallel_timings_naive])
-        writer.writerow([str(i) for i in parallel_timings_thread])
-        writer.writerow([str(i) for i in parallel_spd_naive])
-        writer.writerow([str(i) for i in parallel_eff_naive])
-        writer.writerow([str(i) for i in parallel_spd_thread])
-        writer.writerow([str(i) for i in parallel_eff_thread])
-        f.close()
 
 ###########################
 # HW 1 QUESTION 4
