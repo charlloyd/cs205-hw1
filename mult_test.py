ea7a4024b8a2b8ad84aaa9c60e9d2eaf293a1486
Kareem Carr <kcarr@broadinstitute.org>
2017-03-03 15:37:52 -0500

    .

--- mult_test.py
+++ mult_test.py
@@ -18,8 +18,12 @@ def generate_test_matrices(n):
         n2 = np.random.randint(low=2,high=10)
         n3 = np.random.randint(low=2,high=10)
 
-        A = np.random.normal(size=(n1,n2), dtype=np.float64)
-        B = np.random.normal(size=(n2,n3), dtype=np.float64)
+        A = np.random.normal(size=(n1,n2))
+        B = np.random.normal(size=(n2,n3))
+
+        A = A.astype(np.float64)
+        B = B.astype(np.float64)
+
         C = np.dot(A,B)
 
         matrices+= [(A,B,C)]
@@ -33,7 +37,8 @@ def test_mult_1():
 
     for A,B,C in matrices:
 
-        D = np.zeros_like(C)
+        D = np.zeros_like(C,dtype=np.float64)
+
         hw14.matMult_serial(A,B,D,2)
 
         result = np.mean(np.abs(C-D))
